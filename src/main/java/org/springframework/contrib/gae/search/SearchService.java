f663b1b758c2ad362273379fb79a21d9b2f4d866
Matt Byrne <matt@3wks.com.au>
2019-11-22 07:33:31 +1100

    Fix javadoc

--- src/main/java/org/springframework/contrib/gae/search/SearchService.java
+++ src/main/java/org/springframework/contrib/gae/search/SearchService.java
@@ -268,7 +268,7 @@ public interface SearchService {
      * @param entityClass Class of entity to clear index for.
      * @param <E> Entity type.
      * @param maxDocuments max number of documents to remove (e.g. to avoid API limits).
-     *                     If <=0 then there is no maximum.
+     *                     If less than or equal to 0 then there is no maximum.
      *
      * @return Number of entries removed.
      */
